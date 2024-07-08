3d2ca752207de28ac1a1d9f43041030898883e7f
oleksandr.y <oleksandr.yurchenko@orderbird.com>
2024-07-08 15:53:44 +0200

    Add coverage configuration

--- modules/sonarcloud/main.tf
+++ modules/sonarcloud/main.tf
@@ -7,11 +7,16 @@ terraform {
   }
 }
 
-resource "null_resource" "create_project" {
+resource "null_resource" "create_or_update_project" {
   provisioner "local-exec" {
     command = <<EOT
       curl -u ${var.sonarcloud_api_token}: \
            -X POST "https://sonarcloud.io/api/projects/create" \
+           -d "organization=${var.organization_key}" \
+           -d "name=${var.project_name}" \
+           -d "project=${var.project_key}" || \
+      curl -u ${var.sonarcloud_api_token}: \
+           -X POST "https://sonarcloud.io/api/projects/update" \
            -d "organization=${var.organization_key}" \
            -d "name=${var.project_name}" \
            -d "project=${var.project_key}"
@@ -28,7 +33,7 @@ resource "null_resource" "assign_quality_gate" {
            -d "gateId=${var.quality_gate_id}"
     EOT
   }
-  depends_on = [null_resource.create_project]
+  depends_on = [null_resource.create_or_update_project]
 }
 
 resource "null_resource" "set_permissions" {
@@ -41,7 +46,21 @@ resource "null_resource" "set_permissions" {
            -d "permission=${var.permission}"
     EOT
   }
-  depends_on = [null_resource.create_project]
+  depends_on = [null_resource.create_or_update_project]
+}
+
+resource "null_resource" "run_sonarscanner" {
+  provisioner "local-exec" {
+    command = <<EOT
+      sonar-scanner \
+      -Dsonar.projectKey=${var.project_key} \
+      -Dsonar.organization=${var.organization_key} \
+      -Dsonar.sources=${var.sources} \
+      -Dsonar.tests=${var.tests} \
+      -Dsonar.python.coverage.reportPaths=${var.coverage_report_paths} \
+      -Dsonar.login=${var.sonarcloud_api_token}
+    EOT
+  }
 }
 
 output "project_url" {
