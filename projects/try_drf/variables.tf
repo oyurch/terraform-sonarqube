675339b617c48db67cd8f7d8f3f1593cf63cc0fa
oleksandr.y <oleksandr.yurchenko@orderbird.com>
2024-07-08 14:38:29 +0200

    Add new project to SonarQube

---
+++ projects/try_drf/variables.tf
@@ -0,0 +1,40 @@
+variable "sonarcloud_api_token" {
+  type        = string
+  description = "The API token for SonarCloud"
+}
+
+variable "organization_key" {
+  type        = string
+  description = "The SonarCloud organization key"
+  default     = "oyurch"
+}
+
+variable "project_key" {
+  type        = string
+  description = "The SonarCloud project key"
+  default     = "oyurch_twitter_crawler"
+}
+
+variable "project_name" {
+  type        = string
+  description = "The SonarCloud project name"
+  default     = "Twitter Crawler"
+}
+
+variable "quality_gate_id" {
+  type        = string
+  description = "The SonarCloud quality gate ID"
+  default     = "9"
+}
+
+variable "group_name" {
+  type        = string
+  description = "The name of the group to add permissions to"
+  default     = "members"
+}
+
+variable "permission" {
+  type        = string
+  description = "The permission to add to the group"
+  default     = "admin"
+}
