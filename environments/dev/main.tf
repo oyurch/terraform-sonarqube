d110f8b759873534837559305e1056581d006dfa
Alex Y <40519639+oyurch@users.noreply.github.com>
2024-07-08 15:55:26 +0200

    Add coverage configuration (#8)

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,17 +1,17 @@
 module "twitter_crawler" {
   source               = "../../projects/twitter_crawler"
-  sonarcloud_api_token = var.sonarcloud_api_token
-  organization_key     = var.organization_key
-  quality_gate_id      = var.quality_gate_id
-  group_name           = var.group_name
-  permission           = var.permission
+  sonarcloud_api_token  = var.sonarcloud_api_token
+  organization_key      = var.organization_key
+  quality_gate_id       = var.quality_gate_id
+  group_name            = var.group_name
+  permission            = var.permission
 }
 
 module "try_drf" {
   source               = "../../projects/try_drf"
-  sonarcloud_api_token = var.sonarcloud_api_token
-  organization_key     = var.organization_key
-  quality_gate_id      = var.quality_gate_id
-  group_name           = var.group_name
-  permission           = var.permission
+  sonarcloud_api_token  = var.sonarcloud_api_token
+  organization_key      = var.organization_key
+  quality_gate_id       = var.quality_gate_id
+  group_name            = var.group_name
+  permission            = var.permission
 }
