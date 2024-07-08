6a9b9fb7fb1a93fc76481f1c9e15771b9d5048e2
Alex Y <40519639+oyurch@users.noreply.github.com>
2024-07-08 14:40:58 +0200

    Add new project to SonarQube (#2)

--- projects/try_drf/main.tf
+++ projects/try_drf/main.tf
@@ -2,8 +2,8 @@ module "sonarcloud_project" {
   source               = "../../modules/sonarcloud"
   sonarcloud_api_token = var.sonarcloud_api_token
   organization_key     = var.organization_key
-  project_key          = "oyurch_try_drf"
-  project_name         = "Try Drf"
+  project_key          = "oyurch_twitter_crawler"
+  project_name         = "Twitter Crawler"
   quality_gate_id      = var.quality_gate_id
   group_name           = var.group_name
   permission           = var.permission
