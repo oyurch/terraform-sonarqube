d741ff2c0d57478ad97967ffc98b9525d5fa2a0e
Alex Y <40519639+oyurch@users.noreply.github.com>
2024-07-08 14:47:40 +0200

    Add missing part of the new project (#4)

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -6,3 +6,12 @@ module "twitter_crawler" {
   group_name           = var.group_name
   permission           = var.permission
 }
+
+module "try_drf" {
+  source               = "../../projects/try_drf"
+  sonarcloud_api_token = var.sonarcloud_api_token
+  organization_key     = var.organization_key
+  quality_gate_id      = var.quality_gate_id
+  group_name           = var.group_name
+  permission           = var.permission
+}
