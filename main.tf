3a23bf4505b0abcf0c0769cc121ec77f6769684d
Alex Y. <40519639+oyurch@users.noreply.github.com>
2024-08-14 20:17:36 +0200

    Remove environment folder (#10)

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "twitter_crawler" {
-  source               = "../../projects/twitter_crawler"
+  source               = "./projects/twitter_crawler"
   sonarcloud_api_token  = var.sonarcloud_api_token
   organization_key      = var.organization_key
   quality_gate_id       = var.quality_gate_id
@@ -8,7 +8,7 @@ module "twitter_crawler" {
 }
 
 module "try_drf" {
-  source               = "../../projects/try_drf"
+  source               = "./projects/try_drf"
   sonarcloud_api_token  = var.sonarcloud_api_token
   organization_key      = var.organization_key
   quality_gate_id       = var.quality_gate_id
