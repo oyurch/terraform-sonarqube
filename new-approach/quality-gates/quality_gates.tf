3ec13fe7cf609ab879aca1ebbfb5fa6cba44df26
oleksandr.y <oleksandr.yurchenko@orderbird.com>
2024-08-15 20:32:02 +0200

    revorke a bit

--- new-approach/quality-gates/quality_gates.tf
+++ new-approach/quality-gates/quality_gates.tf
@@ -28,25 +28,32 @@ resource "null_resource" "create_quality_gate" {
     EOT
   }
 }
+
+resource "null_resource" "quality_gate_data" {
+  for_each = var.quality_gates
+
+  triggers = {
+    sonarcloud_token       = var.sonarcloud_api_token
+    sonarcloud_organization = var.sonarcloud_organization
+    gate_name              = each.value.name
+  }
+}
+
 resource "null_resource" "delete_quality_gate" {
   for_each = var.quality_gates
 
   provisioner "local-exec" {
     when = "destroy"
-    environment = {
-      SONARCLOUD_API_TOKEN  = var.sonarcloud_api_token
-      SONARCLOUD_ORGANIZATION = var.sonarcloud_organization
-      GATE_NAME = each.value.name
-    }
     command = <<EOT
-      gate_id=$(curl -s -u $SONARCLOUD_API_TOKEN: "https://sonarcloud.io/api/qualitygates/show" -d "organization=$SONARCLOUD_ORGANIZATION" | jq -r '.qualitygates[] | select(.name=="$GATE_NAME") | .id')
+      gate_id=$(curl -s -u ${self.triggers.sonarcloud_token}: "https://sonarcloud.io/api/qualitygates/show" -d "organization=${self.triggers.sonarcloud_organization}" | jq -r '.qualitygates[] | select(.name=="${self.triggers.gate_name}") | .id')
 
       if [ -n "$gate_id" ]; then
         curl -X POST \
-        -u $SONARCLOUD_API_TOKEN: \
+        -u ${self.triggers.sonarcloud_token}: \
         "https://sonarcloud.io/api/qualitygates/delete" \
         -d "id=$gate_id"
       fi
     EOT
   }
+  depends_on = [null_resource.quality_gate_data]
 }
