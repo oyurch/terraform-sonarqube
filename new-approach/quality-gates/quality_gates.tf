493ee481500ef6abca8ac1fa08a24cea02999270
oleksandr.y <oleksandr.yurchenko@orderbird.com>
2024-08-15 19:21:06 +0200

    Fix conditions format for QG

--- new-approach/quality-gates/quality_gates.tf
+++ new-approach/quality-gates/quality_gates.tf
@@ -13,11 +13,15 @@ resource "null_resource" "create_quality_gate" {
   provisioner "local-exec" {
     when    = create
     command = <<EOT
-      for condition in ${join(" ", each.value.conditions)}; do
+      for condition in ${jsonencode(each.value.conditions)}; do
+        metric=$(echo $condition | jq -r '.metric')
+        operator=$(echo $condition | jq -r '.operator')
+        error=$(echo $condition | jq -r '.error')
+
         curl -X POST \
         -u ${var.sonarcloud_api_token}: \
         "https://sonarcloud.io/api/qualitygates/create_condition" \
-        -d "organization=${var.sonarcloud_organization}&gateName=${each.value.name}&$condition"
+        -d "organization=${var.sonarcloud_organization}&gateName=${each.value.name}&metric=$metric&operator=$operator&error=$error"
       done
     EOT
   }
--- new-approach/terraform.tfvars
+++ new-approach/terraform.tfvars
@@ -4,15 +4,31 @@ quality_gates = {
   gate1 = {
     name = "Quality Gate 1"
     conditions = [
-      "metric=coverage&operator=GT&error=80",
-      "metric=new_coverage&operator=GT&error=80"
+      {
+        metric   = "coverage"
+        operator = "GT"
+        error    = "80"
+      },
+      {
+        metric   = "new_coverage"
+        operator = "GT"
+        error    = "80"
+      }
     ]
   },
   gate2 = {
     name = "Quality Gate 2"
     conditions = [
-      "metric=bugs&operator=LT&error=1",
-      "metric=new_bugs&operator=LT&error=1"
+      {
+        metric   = "bugs"
+        operator = "LT"
+        error    = "1"
+      },
+      {
+        metric   = "new_bugs"
+        operator = "LT"
+        error    = "1"
+      }
     ]
   }
 }
