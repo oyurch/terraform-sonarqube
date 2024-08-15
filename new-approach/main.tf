0ee87b0ea3fa9443e0aca9f165d122f82ad1f65c
oleksandr.y <oleksandr.yurchenko@orderbird.com>
2024-08-15 19:18:04 +0200

    Fix variables names in the main

--- new-approach/main.tf
+++ new-approach/main.tf
@@ -1,7 +1,7 @@
 module "quality_gates" {
   source = "./quality-gates"
 
-  sonarcloud_token        = var.sonarcloud_token
+  sonarcloud_api_token    = var.sonarcloud_api_token
   sonarcloud_organization = var.sonarcloud_organization
   quality_gates           = var.quality_gates
   projects                = var.projects
@@ -10,7 +10,7 @@ module "quality_gates" {
 module "projects" {
   source = "./projects"
 
-  sonarcloud_token        = var.sonarcloud_token
+  sonarcloud_api_token    = var.sonarcloud_api_token
   sonarcloud_organization = var.sonarcloud_organization
   projects                = var.projects
   quality_gates           = var.quality_gates
