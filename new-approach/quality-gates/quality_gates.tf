45b6dc3cb34eeeb32f547a4b06b1c8987ee06ed3
oleksandr.y <oleksandr.yurchenko@orderbird.com>
2024-08-15 20:20:10 +0200

    Create remove quality gate

--- new-approach/quality-gates/quality_gates.tf
+++ new-approach/quality-gates/quality_gates.tf
@@ -27,16 +27,22 @@ resource "null_resource" "create_quality_gate" {
       done
     EOT
   }
+}
+
+resource "null_resource" "delete_quality_gate" {
+  for_each = var.quality_gates
 
   provisioner "local-exec" {
-    when = destroy
+    when = "destroy"
     command = <<EOT
       gate_id=$(curl -s -u ${var.sonarcloud_api_token}: "https://sonarcloud.io/api/qualitygates/show" -d "organization=${var.sonarcloud_organization}" | jq -r '.qualitygates[] | select(.name=="${each.value.name}") | .id')
 
-      curl -X POST \
-      -u ${var.sonarcloud_api_token}: \
-      "https://sonarcloud.io/api/qualitygates/delete" \
-      -d "id=$gate_id"
+      if [ -n "$gate_id" ]; then
+        curl -X POST \
+        -u ${var.sonarcloud_api_token}: \
+        "https://sonarcloud.io/api/qualitygates/delete" \
+        -d "id=$gate_id"
+      fi
     EOT
   }
 }
