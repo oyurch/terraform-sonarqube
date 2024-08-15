3aee9966563b010ce28b040f5d25383de0f15513
oleksandr.y <oleksandr.yurchenko@orderbird.com>
2024-08-15 15:18:54 +0200

    Add Quality gate

--- try-rewe/main.tf
+++ try-rewe/main.tf
@@ -17,3 +17,74 @@ resource "sonarcloud_project" "try_drf" {
   name = "Try DRF"
   visibility = "public"
 }
+
+resource "sonarcloud_project_main_branch" "main" {
+  project_key = "${var.organization_key}_try_drf"
+  name        = "master"
+}
+
+resource "sonarcloud_quality_gate" "backend-python-standard" {
+  name       = "BE Python Standard"
+  is_default = true
+  conditions = [
+    // Less than 100% coverage
+    {
+      metric = "coverage"
+      error  = 100
+      op     = "LT"
+    },
+    // Less than 100% coverage on new code
+    {
+      metric = "new_coverage"
+      error  = 100
+      op     = "LT"
+    },
+    // More than 0.1% duplicated lines
+    {
+      metric = "duplicated_lines_density"
+      error  = 0.1
+      op     = "GT"
+    },
+    // More than 0.1% duplicated lines on new code
+    {
+      metric = "new_duplicated_lines_density"
+      error  = 0.1
+      op     = "GT"
+    },
+    // More than 0 security rating
+    {
+      metric = "security_rating"
+      error  = 0
+      op     = "GT"
+    },
+    // More than 0 security rating on new code
+    {
+      metric = "new_security_rating"
+      error  = 0
+      op     = "GT"
+    },
+    // More than 0 maintainability rating
+    {
+      metric = "maintainability_rating"
+      error  = 0
+      op     = "GT"
+    },
+    // More than 0 maintainability rating on new code
+    {
+      metric = "new_maintainability_rating"
+      error  = 0
+      op     = "GT"
+    },
+  ]
+}
+
+data "sonarcloud_quality_gate" "try_drf" {
+  name = "BE Python Standard"
+}
+
+data "sonarcloud_projects" "all" {}
+
+resource "sonarcloud_quality_gate_selection" "quality_gate_try_drf" {
+  gate_id      = data.sonarcloud_quality_gate.try_drf.gate_id
+  project_keys = [for project in data.sonarcloud_projects.all.projects : project.key if project.name == "Try DRF"]
+}
