1161562c1e39c135daf295730a43e04d105f7841
oleksandr.y <oleksandr.yurchenko@orderbird.com>
2024-08-15 21:34:34 +0200

    Remove bash script

--- new-approach/quality-gates/get_gate_id.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SONARCLOUD_API_TOKEN=$1
-SONARCLOUD_ORGANIZATION=$2
-GATE_NAME=$3
-
-gate_id=$(curl -s -u ${SONARCLOUD_API_TOKEN}: "https://sonarcloud.io/api/qualitygates/show" -d "organization=${SONARCLOUD_ORGANIZATION}" | jq -r '.qualitygates[] | select(.name=="'"$GATE_NAME"'") | .id')
-
-echo "$gate_id"
--- new-approach/quality-gates/quality_gates.tf
+++ new-approach/quality-gates/quality_gates.tf
@@ -5,40 +5,41 @@ resource "null_resource" "manage_quality_gate" {
     sonarcloud_token       = var.sonarcloud_api_token
     sonarcloud_organization = var.sonarcloud_organization
     gate_name              = each.value.name
-    gate_id                = chomp(shell("${path.module}/scripts/get_gate_id.sh", var.sonarcloud_api_token, var.sonarcloud_organization, each.value.name))
   }
 
   provisioner "local-exec" {
     when = create
     command = <<EOT
-      gate_id=${self.triggers.gate_id}
+      gate_id=$(curl -s -u ${self.triggers.sonarcloud_token}: "https://sonarcloud.io/api/qualitygates/create" -d "organization=${self.triggers.sonarcloud_organization}&name=${self.triggers.gate_name}" | jq -r '.id')
 
       if [ -z "$gate_id" ]; then
         echo "Error: Quality gate creation failed"
         exit 1
       fi
 
-      $(for condition in ${jsonencode(each.value.conditions)}; do
+      for condition in $(echo '${jsonencode(each.value.conditions)}' | jq -c '.[]'); do
           metric=$(echo $condition | jq -r '.metric')
           operator=$(echo $condition | jq -r '.operator')
           error=$(echo $condition | jq -r '.error')
 
-          echo curl -X POST \
+          curl -X POST \
             -u ${self.triggers.sonarcloud_token}: \
             "https://sonarcloud.io/api/qualitygates/create_condition" \
             -d "gateId=${gate_id}&metric=${metric}&op=${operator}&error=${error}"
-        done)
+      done
     EOT
   }
 
   provisioner "local-exec" {
     when = destroy
     command = <<EOT
-      if [ -n "${self.triggers.gate_id}" ]; then
+      gate_id=$(curl -s -u ${self.triggers.sonarcloud_token}: "https://sonarcloud.io/api/qualitygates/show" -d "organization=${self.triggers.sonarcloud_organization}" | jq -r '.qualitygates[] | select(.name=="${self.triggers.gate_name}") | .id')
+
+      if [ -n "$gate_id" ]; then
         curl -X POST \
         -u ${self.triggers.sonarcloud_token}: \
         "https://sonarcloud.io/api/qualitygates/destroy" \
-        -d "id=${self.triggers.gate_id}&organization=${self.triggers.sonarcloud_organization}"
+        -d "id=${gate_id}&organization=${self.triggers.sonarcloud_organization}"
       fi
     EOT
   }
