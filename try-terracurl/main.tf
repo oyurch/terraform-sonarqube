d2b6345ef663995d80348da72dea5a124b734b4a
oleksandr.y <oleksandr.yurchenko@orderbird.com>
2024-08-19 14:53:06 +0200

    Fix response_codes

--- try-terracurl/main.tf
+++ try-terracurl/main.tf
@@ -17,14 +17,13 @@ resource "terracurl_request" "create_quality_gate" {
   headers = {
     Authorization = "Basic ${base64encode(var.sonarcloud_api_token)}"
   }
-  body = jsonencode({
+  request_body = jsonencode({
     organization = var.sonarcloud_organization
     name         = each.value.name
   })
 
-  response_code = 200
   name          = ""
-  response_codes = []
+  response_codes = [200]
 }
 
 // trigger build
@@ -37,11 +36,9 @@ resource "terracurl_request" "get_quality_gate_id" {
   headers = {
     Authorization = "Basic ${base64encode(var.sonarcloud_api_token)}"
   }
-  body = jsonencode({
+  request_body = jsonencode({
     organization = var.sonarcloud_organization
   })
-
-  response_code = 200
 
   # Use jq to extract the gate_id from the response
   response_extractor = {
@@ -50,7 +47,7 @@ resource "terracurl_request" "get_quality_gate_id" {
 
   depends_on = [terracurl_request.create_quality_gate]
   name = ""
-  response_codes = []
+  response_codes = [200]
 }
 
 resource "terracurl_request" "add_quality_gate_conditions" {
@@ -63,16 +60,14 @@ resource "terracurl_request" "add_quality_gate_conditions" {
   headers = {
     Authorization = "Basic ${base64encode(var.sonarcloud_api_token)}"
   }
-  body = jsonencode({
+  request_body = jsonencode({
     gateId   = jsonencode(terracurl_request.get_quality_gate_id[each.key].request_body).gate_id
     metric   = each.value.conditions[count.index].metric
     op       = each.value.conditions[count.index].operator
     error    = each.value.conditions[count.index].error
   })
 
-  response_code = 200
-
   depends_on = [terracurl_request.get_quality_gate_id]
   name = ""
-  response_codes = []
+  response_codes = [200]
 }
